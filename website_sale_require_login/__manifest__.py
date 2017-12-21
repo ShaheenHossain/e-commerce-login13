8d5634eccd58f9f04677199fd93ec3966110bb0d
oalca <oscar@vauxoo.com>
2017-12-21 12:23:27 -0600

    [MIG] website_sale_require_login: basic migration v10 to v11

--- website_sale_require_login/__manifest__.py
+++ website_sale_require_login/__manifest__.py
@@ -4,11 +4,12 @@
 {
     "name": "Require login to checkout",
     "summary": "Force users to login for buying",
-    "version": "10.0.1.1.0",
+    "version": "11.0.0.0.0",
     "category": "Website",
     "website": "http://www.antiun.com",
     "author": "Tecnativa, "
               "LasLabs, "
+              "Vauxoo, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
